79dd8d73f606e75a73dd5f564c6ebc918c67ac4d
Kalyani Kadam <kalyanikadam22jan@gmail.com>
2024-06-12 14:11:35 +0530

    express js and react intro code added

---
+++ WPT/Day 13/myfirst/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  var ob={backgroundColor:"blue",color:"white"}
+  return (
+    <div>
+        <h1 style={ob}>Hello World!!</h1>
+        <h2 className="myclass">Welcome to React programming</h2>
+    </div>
+  );
+}
+
+export default App;
